9df538c6815ee982a82d02ffccc764f388acc02d
usamaru64 <VoidCrystallizer@gmail.com>
2021-07-07 07:57:48 +0900

    食べたアイテムが判断できるようになった

--- src/main/kotlin/com/github/usamaru64/orenomeshi/EventListener.kt
+++ src/main/kotlin/com/github/usamaru64/orenomeshi/EventListener.kt
@@ -1,6 +1,8 @@
 package com.github.usamaru64.orenomeshi
 
 import com.github.syari.spigot.api.event.events
+import com.github.syari.spigot.api.item.displayName
+import com.github.syari.spigot.api.string.toColor
 import com.github.usamaru64.orenomeshi.Main.Companion.plugin
 import org.bukkit.event.player.PlayerItemConsumeEvent
 import org.bukkit.potion.PotionEffect
@@ -9,8 +11,10 @@ object EventListener {
     fun register() {
         plugin.events {
             event<PlayerItemConsumeEvent> {
-                when (val player = it.player) {
+                val player = it.player
+                when (it.item) {
                     MyFood.carrot -> {
+                        player.sendMessage("&f${it.item.displayName} &fを食べた".toColor())
                         player.addPotionEffect(PotionEffect(PotionEffectType.LUCK, 120, 5))
                     }
                 }
