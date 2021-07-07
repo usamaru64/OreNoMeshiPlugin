d756459912db973f87386f2df5a29bc8adfdbb69
usamaru64 <VoidCrystallizer@gmail.com>
2021-07-07 16:56:42 +0900

    満腹度が書き換わるイベントをキャンセルして、上書きしてみる

--- src/main/kotlin/com/github/usamaru64/orenomeshi/EventListener.kt
+++ src/main/kotlin/com/github/usamaru64/orenomeshi/EventListener.kt
@@ -4,6 +4,7 @@ import com.github.syari.spigot.api.event.events
 import com.github.syari.spigot.api.item.displayName
 import com.github.syari.spigot.api.string.toColor
 import com.github.usamaru64.orenomeshi.Main.Companion.plugin
+import org.bukkit.event.entity.FoodLevelChangeEvent
 import org.bukkit.event.player.PlayerItemConsumeEvent
 import org.bukkit.potion.PotionEffect
 import org.bukkit.potion.PotionEffectType
@@ -14,6 +15,10 @@ object EventListener {
                 val player = it.player
                 when (it.item) {
                     MyFood.carrot -> {
+                        cancelEventIf<FoodLevelChangeEvent> {
+                            true
+                        }
+                        player.foodLevel + 1
                         player.sendMessage("&f${it.item.displayName} &fを食べた".toColor())
                         player.addPotionEffect(PotionEffect(PotionEffectType.LUCK, 120, 5))
                     }
