c9e3218e5e4877568e7f980a0f2e4c7fa5c54b8a
usamaru64 <VoidCrystallizer@gmail.com>
2021-07-11 14:54:04 +0900

    ポーション等に対応するために食べたときにエフェクトが出ないようにした

--- src/main/kotlin/com/github/usamaru64/orenomeshi/EventListener.kt
+++ src/main/kotlin/com/github/usamaru64/orenomeshi/EventListener.kt
@@ -5,17 +5,19 @@ import com.github.syari.spigot.api.item.displayName
 import com.github.syari.spigot.api.string.toColor
 import com.github.usamaru64.orenomeshi.CustomFood.CustomFood
 import com.github.usamaru64.orenomeshi.Main.Companion.plugin
+import org.bukkit.event.entity.EntityPotionEffectEvent
 import org.bukkit.event.entity.FoodLevelChangeEvent
 import org.bukkit.event.player.PlayerItemConsumeEvent
 import org.bukkit.potion.PotionEffect
 import org.bukkit.potion.PotionEffectType
+
 object EventListener {
     fun register() {
         plugin.events {
-            cancelEventIf<FoodLevelChangeEvent> {
-                CustomFood.container.byName.keys.contains(it.item?.displayName)
-            }
             event<PlayerItemConsumeEvent> {
+                val hasKey = CustomFood.container.byName.keys.contains(it.item.displayName)
+                cancelEventIf<FoodLevelChangeEvent> { hasKey }
+                cancelEventIf<EntityPotionEffectEvent> { hasKey }
                 val player = it.player
                 val name = it.item.displayName
                 val food = CustomFood.getByName(name)
