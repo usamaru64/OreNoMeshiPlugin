b5008e6af6a0b4ddb17816cc17fb773e03513963
usamaru64 <VoidCrystallizer@gmail.com>
2021-07-13 11:31:56 +0900

    イベントの処理を変更してポーション等でエフェクトが付与されないようにした

--- src/main/kotlin/com/github/usamaru64/orenomeshi/EventListener.kt
+++ src/main/kotlin/com/github/usamaru64/orenomeshi/EventListener.kt
@@ -2,11 +2,9 @@ package com.github.usamaru64.orenomeshi
 
 import com.github.syari.spigot.api.event.events
 import com.github.syari.spigot.api.item.displayName
-import com.github.syari.spigot.api.string.toColor
+import com.github.syari.spigot.api.message.sendChatMessage
 import com.github.usamaru64.orenomeshi.CustomFood.CustomFood
 import com.github.usamaru64.orenomeshi.Main.Companion.plugin
-import org.bukkit.event.entity.EntityPotionEffectEvent
-import org.bukkit.event.entity.FoodLevelChangeEvent
 import org.bukkit.event.player.PlayerItemConsumeEvent
 import org.bukkit.potion.PotionEffect
 import org.bukkit.potion.PotionEffectType
@@ -14,17 +12,19 @@ import org.bukkit.potion.PotionEffectType
 object EventListener {
     fun register() {
         plugin.events {
+//            event<FoodLevelChangeEvent> { it.isCancelled = true }
+//            event<EntityPotionEffectEvent> { it.isCancelled = true }
             event<PlayerItemConsumeEvent> {
-                val hasKey = CustomFood.container.byName.keys.contains(it.item.displayName)
-                cancelEventIf<FoodLevelChangeEvent> { hasKey }
-                cancelEventIf<EntityPotionEffectEvent> { hasKey }
+//                val hasKey = CustomFood.container.byName.keys.contains(it.item.displayName)
                 val player = it.player
                 val name = it.item.displayName
                 val food = CustomFood.getByName(name)
                 if (food != null) {
+                    it.isCancelled = true
                     player.saturation += food.saturation
                     player.foodLevel += food.feed
-                    player.sendMessage("&f$name &fを食べた".toColor())
+                    player.inventory.itemInMainHand.amount += -1
+                    player.sendChatMessage("&f$name &fを食べた")
                     player.addPotionEffect(PotionEffect(PotionEffectType.LUCK, 120, 5))
                 }
             }
